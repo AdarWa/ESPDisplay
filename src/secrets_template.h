fed4674350eb159b33c6241c9a02c33ed06a3fa9
AdarWa <adar.wasserman@gmail.com>
2025-05-20 21:16:56 +0300

    Add secrets.h template file, as the original secrets.h is not commited.

---
+++ src/secrets_template.h
@@ -0,0 +1,12 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+//WiFi
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
+
+//MQTT
+#define MQTT_USER "USER"
+#define MQTT_PASSWORD "PASSWORD"
+
+#endif // SECRETS_H
