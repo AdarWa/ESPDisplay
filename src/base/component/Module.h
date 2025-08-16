63bc09c2baf68969c92a1678d3cbd68909db96ae
AdarWa <adar.wasserman@gmail.com>
2025-08-16 17:34:12 +0300

    Remove actions from Module

--- src/base/component/Module.h
+++ src/base/component/Module.h
@@ -1,21 +1,18 @@
 #pragma once
 #include "../Entity.h"
 #include "../states/States.h"
-#include "../states/Actions.h"
 #include "utils/stdutils.h"
 #include <memory>
 
 class Module : public Entity {
 protected:
     std::unique_ptr<States> states_;
-    std::unique_ptr<Actions> actions_;
 
 public:
     Module();
     virtual ~Module() = default;
 
     States* getStates() const;
-    Actions* getActions() const;
 
     virtual void config();
 
